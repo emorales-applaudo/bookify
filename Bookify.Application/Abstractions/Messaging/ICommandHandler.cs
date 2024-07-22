d6938f5f175b213826643484b9ff41d9e66bea66
emorales-applaudo <emorales@applaudostudios.com>
2024-07-22 00:39:16 -0600

    S3.4: Adding the required abstractions for CQRS

---
+++ Bookify.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application.Abstractions.Messaging
+{
+    public interface ICommandHandler<TCommnad> : IRequestHandler<TCommnad, Result>
+        where TCommnad : ICommand
+    {
+    }
+
+    public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+        where TCommand : ICommand<TResponse>
+    {
+    }
+}
--- Bookify.Application/obj/Debug/net8.0/Bookify.Application.AssemblyInfo.cs
+++ Bookify.Application/obj/Debug/net8.0/Bookify.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0967a734ce15e4dc22fb7d9b7049640f21e5c28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b81f500e5437f30d9a00be6aba41e0e32bf196")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
