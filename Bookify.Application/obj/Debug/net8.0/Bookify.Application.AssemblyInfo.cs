7dbbba65707f194d3faf9c5735f166e422a991c7
emorales-applaudo <emorales@applaudostudios.com>
2024-07-22 10:53:09 -0600

    S3.7: Implementing Queries with Dapper for blazing performance

--- Bookify.Application/obj/Debug/net8.0/Bookify.Application.AssemblyInfo.cs
+++ Bookify.Application/obj/Debug/net8.0/Bookify.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df93854d21ed86fef8735bed03b79cbd9911bfa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baaf2a4bf321f2ef3ddd1f26403d40df9716812d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
