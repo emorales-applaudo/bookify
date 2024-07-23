7e2d0915d7c6bb1b46c93f8739f1f934dfa5b6fe
emorales-applaudo <emorales@applaudostudios.com>
2024-07-22 22:41:35 -0600

    S4.8: Implementing repository interfaces using a generic repository

--- Bookify.Infrastructure/obj/Debug/net8.0/Bookify.Infrastructure.AssemblyInfo.cs
+++ Bookify.Infrastructure/obj/Debug/net8.0/Bookify.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e179de134c6e5bd0d31b2cdb157a3426bc635e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1907f4337b32084d5a43826d67897e3d66d181be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
