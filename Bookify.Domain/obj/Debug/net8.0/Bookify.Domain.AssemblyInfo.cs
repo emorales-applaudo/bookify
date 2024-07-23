0fe21e2dd962a8a1b106acfff9bc086636c8b973
emorales-applaudo <emorales@applaudostudios.com>
2024-07-23 09:35:15 -0600

    S5.2: Creating the Presentation project

--- Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
+++ Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277205226e0bdff9a24b62cb117dc85d3c87f50d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcdcaf1782e5853afcb74094c619e8f1f066ec35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
