76106a2aedf2f1523326c01d4af1b283cc1698fa
emorales-applaudo <emorales@applaudostudios.com>
2024-07-25 06:41:49 -0600

    S6.10: Implementing the authentication service

---
+++ Bookify.Infrastructure/Authentication/KeycloakOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Authentication
+{
+    public sealed class KeycloakOptions
+    {
+        public string AdminUrl { get; set; }
+        public string TokenUrl { get; set; }
+        public string AdminClientId { get; set; }
+        public string AdminClientSecret { get; set; }
+        public string AuthClientId { get; set; }
+        public string AuthClientSecret { get; set; }
+    }
+}
